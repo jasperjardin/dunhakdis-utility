adf3083fd571b19475a585b92ef92ae88fd95c2e
Joseph Gabito <codehaiku.io@gmail.com>
2015-11-30 16:53:10 +0800

    Initial Project Commit

---
+++ dunhakdis-utility.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name: Dunhakdis Utility
+ * Description: A helper plug-in that adds "bells and whistles" to variety of Premium WordPress Theme
+ * Version: 1.0
+ * Author: Dunhakdis
+ * Author URI: http://dunhakdis.me
+ * Text Domain: dutility
+ * License: GPL2
+ *
+ * Includes all the file necessary for the plugin.
+ *
+ * PHP version 5
+ *
+ * @package   Dunhakdis Utility
+ * @author    Dunhakdis <noreply@dunhakdis.me>
+ * @copyright 2015 - Dunhakdis 
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link      http://dunhakdis.me
+ * @since     1.0
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+?>
