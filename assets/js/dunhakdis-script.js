69083ce9090ff6b4ea4dde6e9f8d333547ae29f6
Joseph Gabito <codehaiku.io@gmail.com>
2015-12-01 17:10:01 +0800

    added coursel attr

--- assets/js/dunhakdis-script.js
+++ assets/js/dunhakdis-script.js
@@ -8,11 +8,45 @@
 jQuery(document).ready( function($) 
 {
  	'use strict';
+
+ 	/**
+ 	 * This function checks if propValue is undefine or not.
+ 	 * 
+ 	 * @return the propValue if set, otherwise, defaultValue set.
+ 	 */
+ 	var dutility_attr = function( propValue, defaultValue ) {
+ 		return !propValue ? defaultValue : propValue;
+ 	}
+
+ 	var dutility_bool_string = function( string ) {
+ 		return string === "false" ? false: true;
+ 	}
+
+ 	/**
+ 	 * Begin carousel
+ 	 */
+ 	var $carousel = $('.dunhakdis-utility-owl-carousel');
+
+ 	$.each( $carousel, function(){
+
+ 		var prop_items = dutility_attr( $(this).attr('data-items'), 4 );
+ 		var prop_navigation = dutility_attr( $(this).attr('data-navigation'), true );
+ 		var prop_pagination = dutility_attr( $(this).attr('data-pagination'), true );
+
+ 			prop_navigation = dutility_bool_string( prop_navigation );
+ 			prop_pagination = dutility_bool_string( prop_pagination );
+ 		
+
+	  	$(this).owlCarousel({
+	  		navigation: prop_navigation,
+	  		items: prop_items,
+	  		pagination: prop_pagination,
+	  		lazyLoad: true
+	  	});
+
+ 	});
+
  	
-  	$('.dunhakdis-utility-owl-carousel').owlCarousel({
-  		navigation: true,
-  		items: 4,
-  		lazyLoad: true
-  	});
- 
+
+ 	
 });
