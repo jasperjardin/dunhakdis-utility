4f7713d06d2f09644f6f3ac101b97f66976bb396
Joseph Gabito <codehaiku.io@gmail.com>
2015-12-27 00:37:11 +0800

    new update

--- shortcodes/items/team.php
+++ shortcodes/items/team.php
@@ -2,7 +2,106 @@
 add_shortcode( 'dunhakdis_team', 'dunhakdis_team' );
 add_action( 'vc_before_init', 'dunhakdis_team_vc' );
 
+function dunhakdis_team_vc() {
+
+	vc_map( array(
+		"name" => __( "Dunhakdis Team", "dutility" ),
+		"base" => "dunhakdis_team",
+		"class" => "",
+		"category" => __( "Content", "dutility"),
+		"params" => array(
+			array(
+				"type" => "attach_image",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Avatar", "dutility" ),
+				"param_name" => "avatar",
+				"value" => "",
+				"description" => __( "Upload an avatar for this member. The avatar will be automatically resize to 150x150 thumbnail size.", "dutility" ),
+			), //Avatar
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Name", "dutility" ),
+				"param_name" => "name",
+				"value" => "",
+				"description" => __( "The name of the member.", "dutility" ),
+			), //Name
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Title", "dutility" ),
+				"param_name" => "title",
+				"value" => "",
+				"description" => __( "What's the role of this member in your team or company?", "dutility" ),
+			), //Title
+			array(
+				"type" => "textarea",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "About", "dutility" ),
+				"param_name" => "about",
+				"value" => "",
+				"description" => __( "Tell something about this member of your team.", "dutility" ),
+			), //About
+			array(
+				"type" => "vc_link",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Website", "dutility" ),
+				"param_name" => "website",
+				"value" => "",
+				"description" => __( "Enter the website of the member. Leave blank to disable the website link in member's name.", "dutility" ),
+			), //website
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Facebook", "dutility" ),
+				"param_name" => "facebook",
+				"value" => "",
+				"description" => __( "Enter the team member's Facebook Profile Url. Leave blank to disable.", "dutility" ),
+			), //Facebook
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Google+", "dutility" ),
+				"param_name" => "googleplus",
+				"value" => "",
+				"description" => __( "Enter the team member's Google Plus Url. Leave blank to disable.", "dutility" ),
+			), //Google+
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Twitter", "dutility" ),
+				"param_name" => "twitter",
+				"value" => "",
+				"description" => __( "Enter the team member's Twitter Url. Leave blank to disable.", "dutility" ),
+			), //Twitter
+			array(
+				"type" => "textfield",
+				"holder" => "",
+				"class" => "",
+				"heading" => __( "Linkedin", "dutility" ),
+				"param_name" => "linkedin",
+				"value" => "",
+				"description" => __( "Enter the team member's LinkedIn Url. Leave blank to disable.", "dutility" ),
+			), //Linkedin
+		) // params
+   ) ); //vc_map
+}
+
+/**
+ * Callback function
+ * @param  [type] $atts [description]
+ * @return [type]       [description]
+ */
 function dunhakdis_team( $atts ) {
+
 	extract( shortcode_atts( 
 			array(
 	        	'avatar' => '',
@@ -21,132 +120,98 @@ function dunhakdis_team( $atts ) {
 	ob_start();
 	?>
 	<div class="dunhakdis-team-container">
+			
+		<?php if ( is_numeric( $avatar  ) ) { ?>
+
+			<?php $avatar = wp_get_attachment_image_url( intval( $avatar ), 'thumbnail' ); ?>
+	
+		<?php } ?>
+			
 		<div class="dunhakdis-team-avatar">
-			<img src="<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $avatar ) ); ?>" >
+			<img src="<?php echo esc_url( $avatar ); ?>" alt="<?php _e('Profile', 'shoemaker'); ?>">
 		</div>
-		<div class="dunhakdis-team-name">
-			<h3>
-				<?php echo esc_html( apply_filters( 'dunhakdis_team_filter', $name ) ); ?>
+
+		<div class="dunhakdis-team-details">
+
+			<h3 class="dunhakdis-team-name">
+				
+				<?php if ( function_exists( 'vc_build_link') ) { ?>
+					<?php $website = vc_build_link( $website ); ?>
+					<?php if ( is_array( $website ) ) { ?>
+						<?php if ( !empty( $website['url'] ) ) { ?>
+							<?php $website = $website['url']; ?>
+						<?php } ?>
+					<?php } ?>
+				<?php } ?>
+
+				<?php if ( !empty( $website ) ) { ?>
+					<a href="<?php echo esc_url( $website ); ?>" title="<?php echo esc_attr( $name ); ?>">
+				<?php } ?>
+
+					<?php echo esc_html( $name ); ?>
+
+				<?php if ( !empty( $website ) ) { ?>
+					</a>
+				<?php } ?>
 			</h3>
+
 			<h4 class="dunhakdis-team-title">
 				<?php echo esc_html( apply_filters( 'dunhakdis_team_filter', $title ) ); ?>
 			</h4>
-		</div>
-		<div class="dunhakdis-team-social">
-			<a href="https://www.facebook.com/<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $facebook ) ); ?>">
-				<span class="">fb</span>
-			</a>
-			<a href="https://plus.google.com/<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $googleplus ) ); ?>">
-				<span class="">g+</span>
-			</a>
-			<a href="https://twitter.com/<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $twitter ) ); ?>">
-				<span class="">twitter</span>
-			</a>
-			<a href="https://www.linkedin.com/in/<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $linkedin ) ); ?>">
-				<span class="">linkedin</span>
-			</a>
-		</div>
-		<div class="dunhakdis-team-about">
-			<a class="dunhakdis-team-website" href="<?php echo esc_url_raw( apply_filters( 'dunhakdis_team_filter', $website ) ); ?>">
-				<span class="">website</span>
-			</a>
-			<p>
-				<?php echo esc_html( apply_filters( 'dunhakdis_team_filter', $about ) ); ?>
-			</p>
-		</div>
+
+			<div class="dunhakdis-team-social">
+				<?php 
+					$social_media_collection = array(
+						'facebook' => array(
+								'icon' => 'fa fa-facebook',
+								'class' => 'dunhakdis-team-fb',
+								'link' => $facebook,
+								'title' => __('Facebook', 'shoemaker')
+							),
+						'twitter' => array(
+								'icon' => 'fa fa-twitter',
+								'class' => 'dunhakdis-team-tw',
+								'link' => $twitter,
+								'title' => __('Twitter', 'shoemaker')
+							),
+						'googleplus' => array(
+								'icon' => 'fa fa-google-plus',
+								'class' => 'dunhakdis-team-gplus',
+								'link' => $googleplus,
+								'title' => __('Google+', 'shoemaker')
+							),
+						'linkedin' => array(
+								'icon' => 'fa fa-linkedin',
+								'class' => 'dunhakdis-team-linkedin',
+								'link' => $linkedin,
+								'title' => __('LinkedIn', 'shoemaker')
+							),
+						); 
+					?>
+					<ul>
+					<?php
+					foreach( $social_media_collection as $social_media ) { ?>
+						<?php if ( ! empty( $social_media['link'] ) ) { ?>
+							<li>
+								<a class="<?php esc_attr( $social_media['class'] ); ?>" href="<?php echo esc_url( $social_media['link'] ); ?>" title="<?php echo esc_attr( $social_media['title'] ); ?>">
+									<span class="<?php echo esc_attr( $social_media['icon'] ); ?>"></span>
+								</a>
+							</li>
+						<?php } ?>				
+					<?php } ?>
+					</ul>
+
+			</div>
+
+			<div class="dunhakdis-team-about">
+				<p>
+					<?php echo esc_html( $about ); ?>
+				</p>
+			</div>
+
+		</div><!--.dunhakdis-team-details-->
+		
 	</div>
 	<?php
 	return ob_get_clean();
-}
-
-function dunhakdis_team_vc() {
-	vc_map( array(
-		"name" => __( "Dunhakdis Team", "dutility" ),
-		"base" => "dunhakdis_team",
-		"class" => "",
-		"category" => __( "Content", "dutility"),
-		"params" => array(
-			array(
-				"type" => "attach_image",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Avatar", "dutility" ),
-				"param_name" => "avatar",
-				"value" => "",
-				"description" => __( "Input your avatar.", "dutility" ),
-			), //Avatar
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Name", "dutility" ),
-				"param_name" => "name",
-				"value" => "",
-				"description" => __( "Please type in your name.", "dutility" ),
-			), //Name
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Title", "dutility" ),
-				"param_name" => "title",
-				"value" => "",
-				"description" => __( "Please type in your Title.", "dutility" ),
-			), //Title
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "About", "dutility" ),
-				"param_name" => "about",
-				"value" => "",
-				"description" => __( "Write something about yourself.", "dutility" ),
-			), //About
-			array(
-				"type" => "vc_link",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Website", "dutility" ),
-				"param_name" => "website",
-				"value" => "",
-				"description" => __( "Write your Website here.", "dutility" ),
-			), //website
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Facebook", "dutility" ),
-				"param_name" => "facebook",
-				"value" => "",
-				"description" => __( "Write your Facebook here.", "dutility" ),
-			), //Facebook
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Google+", "dutility" ),
-				"param_name" => "googleplus",
-				"value" => "",
-				"description" => __( "Write your Google+ here.", "dutility" ),
-			), //Google+
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Twitter", "dutility" ),
-				"param_name" => "twitter",
-				"value" => "",
-				"description" => __( "Write your Twitter here.", "dutility" ),
-			), //Twitter
-			array(
-				"type" => "textfield",
-				"holder" => "",
-				"class" => "",
-				"heading" => __( "Linkedin", "dutility" ),
-				"param_name" => "linkedin",
-				"value" => "",
-				"description" => __( "Write your Linkedin here.", "dutility" ),
-			), //Linkedin
-		) // params
-   ) ); //vc_map
 }
