61834a63e50dd8df2a6d1933b449f09534977a2a
Joseph Gabito <codehaiku.io@gmail.com>
2016-01-19 17:37:07 +0800

    Fixed data-items

--- assets/js/dunhakdis-script.js
+++ assets/js/dunhakdis-script.js
@@ -5,13 +5,13 @@
  *
  * @since 1.0
  */
-jQuery(document).ready( function($) 
+jQuery(document).ready( function($)
 {
  	'use strict';
 
  	/**
  	 * This function checks if propValue is undefine or not.
- 	 * 
+ 	 *
  	 * @return the propValue if set, otherwise, defaultValue set.
  	 */
  	var dutility_attr = function( propValue, defaultValue ) {
@@ -32,10 +32,9 @@ jQuery(document).ready( function($)
  		var prop_items = dutility_attr( $(this).attr('data-items'), 4 );
  		var prop_navigation = dutility_attr( $(this).attr('data-navigation'), true );
  		var prop_pagination = dutility_attr( $(this).attr('data-pagination'), true );
-
  			prop_navigation = dutility_bool_string( prop_navigation );
  			prop_pagination = dutility_bool_string( prop_pagination );
- 		
+
 	  	$(this).owlCarousel({
 	  		navigation: prop_navigation,
 	  		items: prop_items,
@@ -57,5 +56,5 @@ jQuery(document).ready( function($)
 				}
 			});
  		});
- 	
+
 });
